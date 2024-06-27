c179bd0d529d027b4e8b18df57e1e4c48bef38ad
remiborris <remiborris@gmail.com>
2024-06-27 19:19:01 -0300

    fix the code and add more test code

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +31,30 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['L', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['G', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['T', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LIGHT');
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if no matrix is given", function() {
+    const result = wordSearch([[]], 'WORD');
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,38 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (!letters) {
+    return false;
+  }
+  const arrayFlip = [];
+  for (let i = 0; i < letters.length; i++) {
+    arrayFlip.push([]);
+  }
+  letters.forEach((subArray, xIndex) => {
+    subArray.forEach((letter, yIndex) => {
+      arrayFlip[yIndex][xIndex] = letter;
+    });
+  });
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  const verticalJoin = arrayFlip.map(ls => ls.join(''));
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+// let matrix = [
+//   ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//   ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//   ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//   ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//   ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//   ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//   ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//   ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//   ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+// ];
+
+// wordSearch(matrix,'FRANK');
+module.exports = wordSearch;
